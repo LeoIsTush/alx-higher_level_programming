f09f3b27c99e9c5ce3988554422d3549ec4122c0
LeoIsTush <122827965+LeoIsTush@users.noreply.github.com>
2023-08-16 19:25:40 +0100

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- list all databases of the MySQL server
-SHOW DATABASES;
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
